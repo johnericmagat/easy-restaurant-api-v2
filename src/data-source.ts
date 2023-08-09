3df957ff231f20c39db6093b57e8f7d81c3429ee
John Eric Magat <johnericmagat@gmail.com>
2023-08-09 11:43:31 +0800

    MySql support

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,18 +2,34 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
+//MSSQL
+// export const AppDataSource = new DataSource({
+//     type: "mssql",
+//     host: "localhost",
+//     username: "sa",
+//     password: "P@ssword1234",
+//     database: "easypos",
+//     synchronize: true,
+//     logging: false,
+//     entities: [User],
+//     migrations: [],
+//     subscribers: [],
+//     extra: {
+//         trustServerCertificate: true,
+//       }
+// })
+
+//MYSQL
 export const AppDataSource = new DataSource({
-    type: "mssql",
-    host: "localhost",
-    username: "sa",
-    password: "P@ssword1234",
-    database: "easypos",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-    extra: {
-        trustServerCertificate: true,
-      }
+  type: "mysql",
+  host: "localhost",
+  port: 3309,
+  username: "root",
+  password: "P@ssword1234",
+  database: "easypos",
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
 })
