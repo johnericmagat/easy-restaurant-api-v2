09546bfaeedc4cc5542ca97b0f9e413dbe17b507
John Eric Magat <johnericmagat@gmail.com>
2023-08-09 11:52:25 +0800

    Sqlite support

--- src/data-source.ts
+++ src/data-source.ts
@@ -20,13 +20,24 @@ import { User } from "./entity/User"
 // })
 
 //MYSQL
+// export const AppDataSource = new DataSource({
+//   type: "mysql",
+//   host: "localhost",
+//   port: 3309,
+//   username: "root",
+//   password: "P@ssword1234",
+//   database: "easypos",
+//   synchronize: true,
+//   logging: false,
+//   entities: [User],
+//   migrations: [],
+//   subscribers: [],
+// })
+
+//SQLITE
 export const AppDataSource = new DataSource({
-  type: "mysql",
-  host: "localhost",
-  port: 3309,
-  username: "root",
-  password: "P@ssword1234",
-  database: "easypos",
+  type: "sqlite",
+  database: "easypos_local.db",
   synchronize: true,
   logging: false,
   entities: [User],
